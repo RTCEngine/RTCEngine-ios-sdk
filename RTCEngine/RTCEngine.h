eaf3b2e80dd891833648b10fe65f6239735881cb
notedit <notedit@gmail.com>
2018-08-03 19:51:16 +0800

    create framework

---
+++ RTCEngine/RTCEngine.h
@@ -0,0 +1,19 @@
+//
+//  RTCEngine.h
+//  RTCEngine
+//
+//  Created by xiang on 03/08/2018.
+//  Copyright © 2018 RTCEngine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RTCEngine.
+FOUNDATION_EXPORT double RTCEngineVersionNumber;
+
+//! Project version string for RTCEngine.
+FOUNDATION_EXPORT const unsigned char RTCEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RTCEngine/PublicHeader.h>
+
+
